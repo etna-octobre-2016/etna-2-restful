d2d848fce5c9b9ff8a613014bf29fbe9cbd97402
Said AHEMT <said.ahemt@nolaroads.com>
2014-10-31 17:38:08 +0100

    ajoute le parsing du fichier de configuration de l'application

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 require 'vendors'.DIRECTORY_SEPARATOR.'autoload.php';
 
 $app = new App\Application();
+
--- src/App/Application.php
+++ src/App/Application.php
@@ -1,13 +1,51 @@
 <?php namespace App;
 
-use DIRECTORY_SEPARATOR as DS;
-use Silex\Application as SilexApp;
-
-class Application extends SilexApp
+class Application
 {
-    function __construct(ArrayAccess $config = null)
+    private $silexApplication;
+    private $cfg;
+
+    /* Méthodes magiques */
+
+    function __construct()
     {
-        parent::__construct();
-        exit('app init biatch test');
+        $this->_fetchConfiguration('config'.DIRECTORY_SEPARATOR.'app.json');
+        $this->_initSilexApplication();
+    }
+
+    /* Méthodes publiques */
+
+    /**
+     * Retourne la configuration de l'application
+     * @param  string $sectionName  (optionel) une section spécifique du fichier de configuration
+     * @return mixed                retourne la configuration ou false en cas d'erreur
+     */
+    public function config($sectionName = null)
+    {
+        if (!is_string($sectionName))
+        {
+            return $this->cfg;
+        }
+        if (property_exists($this->cfg, $sectionName))
+        {
+            return $this->cfg->$sectionName;
+        }
+        return false;
+    }
+
+    /* Méthodes privées */
+
+    private function _initSilexApplication()
+    {
+        $this->silexApplication = new \Silex\Application();
+    }
+    private function _fetchConfiguration($filename)
+    {
+        $this->cfg = null;
+
+        if (file_exists($filename))
+        {
+            $this->cfg = json_decode(file_get_contents($filename));
+        }
     }
 }
