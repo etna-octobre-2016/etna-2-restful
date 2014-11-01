a8e8a62d87decffe67895073491406990b9936da
Said AHEMT <said.ahemt@nolaroads.com>
2014-11-01 15:41:06 +0100

    ajoute la requete sql

--- src/App/Controllers/UsersController.php
+++ src/App/Controllers/UsersController.php
@@ -2,6 +2,8 @@
 
 use App\Application as Application;
 use App\Interfaces\iController as iController;
+use PDO;
+use PDOException;
 use Symfony\Component\HttpFoundation\Request as SilexRequest;
 use Symfony\Component\HttpFoundation\Response as SilexResponse;
 
@@ -9,19 +11,21 @@ class UsersController implements iController
 {
     static public function get(Application $app, SilexRequest $request, $id)
     {
-
-        var_dump($app);
-
-        echo '--------------------<br>';
-
-        var_dump($request);
-
-        echo '--------------------<br>';
-
-        var_dump($id);
-
-        echo '--------------------<br>';
-
-        return new SilexResponse('test titi', 200);
+        try{
+            $sql = 'SELECT * FROM user WHERE id = :id';
+            $params = [ ':id' => (int)$id ];
+            $types = [PDO::PARAM_INT];
+            $pdoStatement = $app->db->executeQuery($sql, $params, $types);
+            $result = $pdoStatement->fetch();
+            if ($result !== false)
+            {
+                return new SilexResponse('test titi', 200);
+            }
+            return new SilexResponse('not found', 404);
+        }
+        catch (PDOException $e){
+            $app->logger->addFatal($e->getMessage());
+            return new SilexResponse('an error occured', 500);
+        }
     }
 }
