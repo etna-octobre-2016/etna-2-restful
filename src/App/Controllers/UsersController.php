be33f9eafe8c67f3196dafc87fce8514eea6e1b7
niou128 <niou128@gmail.com>
2014-11-05 10:52:54 +0100

    l'appel à POST fonctionne. Test d'insertion d'un user.

--- src/App/Controllers/UsersController.php
+++ src/App/Controllers/UsersController.php
@@ -40,9 +40,9 @@ class UsersController implements iController
     {
         $json = file_get_contents('php://input');
         $obj = json_decode($json);
-        $format = 'json';
-        $headers = ['Content-Type' => 'application/json'];
-        return new SilexResponse($app->serialize(['status' => 200, 'message' => 'test'], $format), 200, $headers);
+        // $format = 'json';
+        // $headers = ['Content-Type' => 'application/json'];
+        // return new SilexResponse($app->serialize(['status' => 200, 'message' => 'test'], $format), 200, $headers);
         try{
             $sql = 'INSERT into user (lastname, firstname, email, password, role) VALUES (:lastname, :firstname, :email, :password, :role)';
             $params = [':lastname' => $obj->{'lastname'}, ':firstname' => $obj->{'firstname'}, ':email' => $obj->{'email'}, ':password' => $obj->{'password'}, ':role' => $obj->{'role'}];
