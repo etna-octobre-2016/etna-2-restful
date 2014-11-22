e6e9188d3a16d758d18bb4d187e871d355a50312
niou128 <niou128@gmail.com>
2014-11-22 15:38:37 +0100

    message inutile mais chargé d'espoir

--- src/App/Controllers/UsersController.php
+++ src/App/Controllers/UsersController.php
@@ -231,7 +231,7 @@ class UsersController extends ApplicationController
                 $app->serialize(
                     [
                         'status'    => self::STATUS_OK,
-                        'message'   => $user->get('role'),
+                        'message'   => $request->request->get('role'),
                     ],
                     $format
                 ),
